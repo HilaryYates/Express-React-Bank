8cc9480f46464ba47e289a73df60ae84eafd31ce
hilaryyates <hilryyates@gmail.com>
2021-03-19 20:46:26 -0400

    first commit

--- src/client/Add.jsx
+++ src/client/Add.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 class Add extends Component {
   constructor(props) {
     super(props);
-    this.state = { description: "", amount: 0 };
+    this.state = { description: "", amount: 3 };
   }
   handleDescription = (evt) => {
     // console.log(evt.target.value);
@@ -12,8 +12,29 @@ class Add extends Component {
   handleAmount = (evt) => {
     const amount = parseInt(evt.target.value);
     // console.log(amount);
-    this.setState({ amount: amount });
+    // this.setState({ amount: amount });
     // console.log(this.state.amount);
+  };
+  handleSubmit = (evt) => {
+    evt.preventDefault();
+    let form = new FormData();
+    // console.log(form);
+    form.append("amount", this.state.amount);
+    // for (var pair of form.entries()) {
+    //   console.log(pair[0] + ", " + pair[1]);
+    // }
+    fetch("http://localhost:8080/add", {
+      method: "POST",
+      // headers: {
+      //   cors: `Access-Control-Allow-Origin`,
+      //   "Content-Type": "application/json",
+      // },
+      body: JSON.stringify(form),
+      // headers: {
+      // "Content-Type": "application/json",
+      //   "Access-Control-Allow-Origin": "http://localhost:8080/add",
+      // },
+    });
   };
   render = () => {
     return (
--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,21 @@
 const express = require("express");
+
 const app = express();
 
 app.use(express.static("dist"));
-
-app.listen(process.env.PORT || 8080, () =>
-  console.log(`Listening on port ${process.env.PORT || 8080}!`)
-);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// const bodyParser = require("body-parser");
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser());
+// app.use(
+//   express.urlencoded({
+//     extended: true,
+//   })
+// );
+// let fetch = require("node-fetch");
+// let cors = require("cors");
+// app.use(cors());
 
 const account = (() => {
   let transactions;
@@ -15,5 +25,11 @@ const account = (() => {
 })();
 transactions = [];
 
-//app.post("/add", (req, res) => {
-//});
+app.post("/add", (req, res) => {
+  console.log(typeof req.body, req.body, Object.keys(req.body).length === 0);
+  console.log(req.body.amount);
+});
+
+app.listen(process.env.PORT || 8080, () =>
+  console.log(`Listening on port ${process.env.PORT || 8080}!`)
+);
